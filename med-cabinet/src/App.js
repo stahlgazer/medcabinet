b41b91bb87ccfc4a84d84ec01310502ab6c378e0
gavin stahl <stahlgavin@gmail.com>
2020-02-29 19:16:58 -0800

    initial app, dependencies, components/files

---
+++ med-cabinet/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link, NavLink, Redirect } from "react-router-dom";
+// import Contact from './components/Contact';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      test
+    </div>
+    </Router>
+  );
+}
+
+export default App;
