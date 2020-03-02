4efc4802d518264bf3ef7482a92a6d04920b1f55
gavin stahl <stahlgavin@gmail.com>
2020-03-01 18:54:02 -0800

    router, axiosWithAuth, PrivateRoute

--- med-cabinet/src/App.js
+++ med-cabinet/src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch, Link, NavLink, Redirect } from "react-router-dom";
+import { Route, Switch, Link, NavLink, Redirect } from "react-router-dom";
 // import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
     <div className="App">
       test
     </div>
-    </Router>
   );
 }
 
