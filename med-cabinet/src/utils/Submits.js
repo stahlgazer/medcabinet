b51f18863c2727effb7c530897ae9734d87737c6
gavin stahl <stahlgavin@gmail.com>
2020-03-03 23:24:48 -0800

    fixed login routing to show up on home page with no auth

--- med-cabinet/src/utils/Submits.js
+++ med-cabinet/src/utils/Submits.js
@@ -1,6 +1,16 @@
+import axiosWithAuth from './axiosWithAuth';
+
 export const addSubmit = (e, props, elem) => {
     e.preventDefault();
     //post here
+    axiosWithAuth()
+    .post(`/users/${localStorage.getItem('ID')}`, elem)
+    .then(response => {
+      console.log("user data put:", response.data);
+    })
+    .catch(error => {
+      console.log("user data put:", error);
+    });
     props.addFavorite(elem)
 }
 
