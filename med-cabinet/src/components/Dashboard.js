602b63cc045f41ef71537588401d9a3e0d4127bd
gavin stahl <stahlgavin@gmail.com>
2020-03-04 10:59:04 -0800

    dashboard update and favorite submits setup

--- med-cabinet/src/components/Dashboard.js
+++ med-cabinet/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axiosWithAuth from "../utils/axiosWithAuth";
-import {Button} from '@material-ui/core';
+import { Button } from "@material-ui/core";
 
 export default function Dashboard(props) {
   // console.log( 'dashboard props', props)
@@ -37,16 +37,13 @@ export default function Dashboard(props) {
   };
 
   const handleSubmit = event => {
-    setChange(true)
+    setChange(true);
     event.preventDefault();
     preferences.tolerance = Number(preferences.tolerance);
     console.log(preferences, "preferences at time of post 4");
 
     axiosWithAuth()
-      .put(
-        `/users/${localStorage.getItem("ID")}`,
-        preferences
-      )
+      .put(`/users/${localStorage.getItem("ID")}`, preferences)
       .then(response => {
         console.log("Successful update: ", response.data);
       })
@@ -59,77 +56,75 @@ export default function Dashboard(props) {
 
   return (
     <div>
-      {/* create form to update user preferences */}
+      <h2 className="gavforms">{preferences.username} Dashboard</h2>
       <form className="gavforms" onSubmit={handleSubmit} name="register">
-      <h3>Update Your Preferences</h3>
-      <label>Recreational or Medical Use: </label>
-      <select
-        className="gavinputs"
-        name="medicinalUse"
-        value={preferences.medicinalUse}
-        onChange={handleChange}
-      >
-        <option name="medicinalUse" value={false}>
-          Recreational
-        </option>
-        <option name="medicinalUse" value={true}>
-          Medicinal
-        </option>
-      </select>
+        <h3>Update Your Preferences</h3>
+        <label>Recreational or Medical Use: </label>
+        <select
+          className="gavinputs"
+          name="medicinalUse"
+          value={preferences.medicinalUse}
+          onChange={handleChange}
+        >
+          <option name="medicinalUse" value={false}>
+            Recreational
+          </option>
+          <option name="medicinalUse" value={true}>
+            Medicinal
+          </option>
+        </select>
 
-      <label htmlFor="tolerance">Tolerance Level: </label>
-      <select
-        required
-        className="gavinputs"
-        name="tolerance"
-        value={preferences.tolerance}
-        onChange={handleChange}
-      >
-        <option name="tolerance" value={0}>
-          None
-        </option>
-        <option name="tolerance" value={1}>
-          Beginner
-        </option>
-        <option name="tolerance" value={2}>
-          Intermediate
-        </option>
-        <option name="tolerance" value={3}>
-          Regular
-        </option>
-        <option name="tolerance" value={4}>
-          High
-        </option>
-      </select>
+        <label htmlFor="tolerance">Tolerance Level: </label>
+        <select
+          required
+          className="gavinputs"
+          name="tolerance"
+          value={preferences.tolerance}
+          onChange={handleChange}
+        >
+          <option name="tolerance" value={0}>
+            None
+          </option>
+          <option name="tolerance" value={1}>
+            Beginner
+          </option>
+          <option name="tolerance" value={2}>
+            Intermediate
+          </option>
+          <option name="tolerance" value={3}>
+            Regular
+          </option>
+          <option name="tolerance" value={4}>
+            High
+          </option>
+        </select>
 
-      <label htmlFor="medicalConditions">Medical Conditions: </label>
-      <input
-        className="gavinputs"
-        type="text"
-        name="medicalConditions"
-        placeholder="List anything that should be taken into consideration"
-        value={preferences.medicalConditions}
-        onChange={handleChange}
-      />
+        <label htmlFor="medicalConditions">Medical Conditions: </label>
+        <input
+          className="gavinputs"
+          type="text"
+          name="medicalConditions"
+          placeholder="List anything that should be taken into consideration"
+          value={preferences.medicalConditions}
+          onChange={handleChange}
+        />
 
-      <label htmlFor="desiredEffect">Desired Effect: </label>
-      <input
-        required
-        className="gavinputs"
-        type="text"
-        name="desiredEffect"
-        placeholder="Pain Relief, Sleep, Release Stress, Depressant"
-        value={preferences.desiredEffect}
-        onChange={handleChange}
-      />
+        <label htmlFor="desiredEffect">Desired Effect: </label>
+        <input
+          required
+          className="gavinputs"
+          type="text"
+          name="desiredEffect"
+          placeholder="Pain Relief, Sleep, Release Stress, Depressant"
+          value={preferences.desiredEffect}
+          onChange={handleChange}
+        />
 
-      <Button variant="contained" color="primary" type="submit">
-        update
-      </Button>
-      {error && <p>{error}</p>}
-    </form>
-      {preferences.username}
-
+        <Button variant="contained" color="primary" type="submit">
+          update
+        </Button>
+        {error && <p>{error}</p>}
+      </form>
 
       {/* display saved favorites and add delete favorites */}
     </div>
--- med-cabinet/src/utils/Submits.js
+++ med-cabinet/src/utils/Submits.js
@@ -19,5 +19,13 @@ export const addSubmit = (e, props, elem) => {
 export const delSubmit = (e, props, elem) => {
     e.preventDefault();
     //delete here
+    axiosWithAuth()
+    .delete(`/users/${localStorage.getItem('ID')}`, elem)
+    .then(response => {
+      console.log("user data put:", response.data);
+    })
+    .catch(error => {
+      console.log("user data put:", error);
+    });
     props.removeFavorite(elem)
 }
